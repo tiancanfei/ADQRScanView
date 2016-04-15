c99eea6256c27cbd1ac75013a3ce8bf49ee9f88c
安德航 <bjwltiankong@163.com>
2016-04-15 22:51:17 +0800

    Initial Commit

---
+++ ADQRScanViewExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ADQRScanViewExample
+//
+//  Created by bjwltiankong on 16/4/15.
+//  Copyright © 2016年 bjwltiankong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
